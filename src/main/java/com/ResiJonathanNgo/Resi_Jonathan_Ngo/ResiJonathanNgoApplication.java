37741e7ccc39d983b726e5d1faf964cfca8cb5c7
Jonathan Ngo <75549736+JonathanTNgo@users.noreply.github.com>
2025-04-11 22:05:47 -0500

    Spring Boot initial commit

---
+++ src/main/java/com/ResiJonathanNgo/Resi_Jonathan_Ngo/ResiJonathanNgoApplication.java
@@ -0,0 +1,13 @@
+package com.ResiJonathanNgo.Resi_Jonathan_Ngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiJonathanNgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiJonathanNgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ResiJonathanNgo/Resi_Jonathan_Ngo/ResiJonathanNgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ResiJonathanNgo.Resi_Jonathan_Ngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiJonathanNgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
